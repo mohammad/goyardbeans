4cd1c42a4ee1690630ef952a57f76041f25d249a
mohammad-gitup <mohammadsyed.kc@gmail.com>
2020-07-16 22:37:28 -0500

    Remove the extra stuff

--- server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from photoshop import crop_photo
-app = Flask(__name__)
-
-@app.route('/test')
-def hello_world():
-    crop_photo("https://api.time.com/wp-content/uploads/2015/02/imsis270-064.jpg?w=800&quality=85")
-    return 'Hello, World!'
